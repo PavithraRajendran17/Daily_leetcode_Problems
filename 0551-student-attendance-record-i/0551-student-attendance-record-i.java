2dc70a7dbe66389b4d9cf301e7cc492230e0ac78
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-08-07 22:06:45 +0530

    Time: 0 ms (100%), Space: 42.8 MB (62.32%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int absent = 0;
+        int late = 0;
+        for (char ch : s.toCharArray()) {
+
+            if (ch == 'A') {
+                absent++;
+                late = 0;       
+            }
+            else if (ch == 'L') {
+                late++;
+
+                if (late == 3)
+                    return false;
+            }
+            else {             
+                late = 0;
+            }
+
+            if (absent >= 2)
+                return false;
+        }
+
+        return true;
+        
+    }
+}
