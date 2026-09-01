3cfbcec4d065436cd5b951ad3aba2ae8e65ac691
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-09-01 16:32:24 +0530

    Time: 0 ms (100%), Space: 44.8 MB (6.32%) - LeetHub

---
+++ 1560-most-visited-sector-in-a-circular-track/1560-most-visited-sector-in-a-circular-track.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> mostVisited(int n, int[] rounds) {
+        int start = rounds[0];
+        int end = rounds[rounds.length - 1];
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (start <= end) {
+            for (int i = start; i <= end; i++) {
+                ans.add(i);
+            }
+        } 
+        else {
+            for (int i = 1; i <= end; i++) {
+                ans.add(i);
+            }
+
+            for (int i = start; i <= n; i++) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+        
+    }
+}
