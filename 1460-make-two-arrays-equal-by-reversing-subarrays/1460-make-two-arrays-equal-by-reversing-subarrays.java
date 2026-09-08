e47180d50c5d89dfc9def82f980b7aee6c3fdc5c
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-09-08 20:24:11 +0530

    Time: 2 ms (86.36%), Space: 46.4 MB (70.64%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] count = new int[1001];
+        for (int i = 0; i < target.length; i++) {
+            count[target[i]]++;
+            count[arr[i]]--;
+        }
+        for (int i = 1; i <= 1000; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
