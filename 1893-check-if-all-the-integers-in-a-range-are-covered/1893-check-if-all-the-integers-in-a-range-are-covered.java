33f93ed8c9777b09ff6f0e6f6da529c27edd3a1b
PAVITHRA RAJENDRAN <pavithrapanimalar30@gmail.com>
2026-09-25 19:27:54 +0530

    Time: 0 ms (100%), Space: 43.5 MB (46.3%) - LeetHub

---
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isCovered(int[][] ranges, int left, int right) {
+          for (int i = left; i <= right; i++) {
+            boolean covered = false;
+
+            for (int[] range : ranges) {
+                if (range[0] <= i && i <= range[1]) {
+                    covered = true;
+                    break;
+                }
+            }
+
+            if (!covered) {
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+}
